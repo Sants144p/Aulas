ba272c859e8a45bcc455046ac422376046741163
Sants144p <gabriel.moreira.102007@gmail.com>
2024-03-04 08:34:22 -0300

    imc

---
+++ Aula04/imc.js
@@ -0,0 +1,49 @@
+function submeterCalculo() {
+    let confirma = confirm("Você gostaria de prosseguir com a ação?");
+
+    let imc = Math.ceil(document.getElementById("massa").value / document.getElementById("alt").value**2)
+    
+    let classe = "Média"
+
+    if (imc < 18.5){
+
+        classe = "Magreza"
+    }
+
+    else if (18.5 < imc < 24.9){
+
+        classe = "Normal"
+    }
+
+    else  if (24.9 < imc < 29.9){
+
+        classe = "Sobrepeso"
+    }
+
+    else if (29.9 < imc < 39.9){
+
+        classe = "Obesidade"
+    }
+
+    else if (imc > 39.9){
+
+        classe = "Obesidade Grave"
+    }
+
+    if (confirma == true) {
+    let massa = document.getElementById("massa").value;
+    let alt = document.getElementById("alt").value;
+    
+    document.getElementById("resultado").innerHTML = 
+        "<h3>Informações Recebidas</h3>" +
+        "<p>Massa: " + massa + "</p>" +
+        "<p>Altura: " + alt + "</p>" +
+        "<p>IMC: " + imc + "</p>"  +
+        "<p>Classificação: " + classe + "</p>" ;
+
+    } else {
+
+        document.getElementById("resultado").InnerHTML =
+            "<h2>Ação Cancelada</h2>";
+    }
+}
