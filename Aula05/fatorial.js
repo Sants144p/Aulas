13abf3c2306aaaa4c7d11141a7d4d9c94e4d9d90
Sants144p <gabriel.moreira.102007@gmail.com>
2024-03-15 07:59:19 -0300

    Fatorial

---
+++ Aula05/fatorial.js
@@ -0,0 +1,17 @@
+function fatorial(n) {
+    
+    let fat = 1;
+    for (let i=2; i<=n; i++)
+        fat = fat * i
+    return fat;
+}
+
+function fatorial() {
+
+    let n = document.getElementById("numero").value;
+    let fat = 1;
+    for (let i=2; i<=n; i++)
+        fat = fat * i
+    document.getElementById("resultado").innerHTML =
+        `Fatorial(${n}) = ${fat}`;
+}
