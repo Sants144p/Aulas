933d164c02c109a4c82db8db372cfca6717a823e
Sants144p <gabriel.moreira.102007@gmail.com>
2024-07-12 08:21:07 -0300

    frutas

---
+++ Greve/fruta.js
@@ -0,0 +1,33 @@
+let frutas = ["tamarindo","cajá","tomate"]
+
+console.log(frutas)
+
+console.log("")
+
+console.log(frutas[0])
+
+frutas.push("melão")
+frutas.unshift("abacate")
+
+console.log("")
+
+console.log(frutas)
+
+frutas.splice(2,1)
+
+console.log("")
+
+console.log(frutas[0])
+
+console.log("")
+
+console.log(frutas.toString())
+
+console.log("")
+
+for (i=0; i<frutas.length; i++){
+
+    console.log(frutas[i])
+
+}
+
