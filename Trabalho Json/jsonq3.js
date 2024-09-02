b4839ec55f650737615d2e5eba11206ccb01e339
Sants144p <gabriel.moreira.102007@gmail.com>
2024-09-02 07:30:54 -0300

    flags

--- Trabalho Json/jsonq3.js
+++ Trabalho Json/jsonq3.js
@@ -2,11 +2,24 @@ function buscarJogos(){
     const nomeEquipe = document.getElementById('equipe').value
     console.log(nomeEquipe)
     if (!nomeEquipe) {
-        alert('Por favor, digite o nome da equipe.');
+        document.getElementById('resultado').innerHTML = '<p class="error">Por favor, selecione uma equipe.</p>';
         return;
     }
 
     const url_jogos = 'https://worldcupjson.net/matches';
+    const arq_bandeiras = 'flags.json'
+
+    fetch(arq_bandeiras)
+        .then(response => response.json())
+        .then(data => {
+            const bandeiraUrl = data[nomeEquipe];
+            if (bandeiraUrl) {
+                document.getElementById('flag').src = bandeiraUrl;
+            } else {
+                document.getElementById('flag').src = ''; // Limpa a imagem se a equipe não for encontrada
+                alert('Bandeira não encontrada para a equipe especificada.');
+            }
+        })
 
     fetch(url_jogos)
         .then(response => response.json())
@@ -17,17 +30,24 @@ function buscarJogos(){
 
     const result = jogosEquipe.map(jogo => {
         return `
-            <div>
+        <div>
+            Data: ${new Date(jogo.datetime).toLocaleString('pt-BR')} </br>
             Data: ${jogo.datetime} </br>
             Disputa: ${jogo.home_team.name} x ${jogo.away_team.name} </br>
-            Placar: ${jogo.home_score} - ${jogo.away_score} </br>
+            Placar: ${jogo.home_team.goals} - ${jogo.away_team.goals} </br>
+            Penaltis: ${jogo.home_team.penalties} - ${jogo.away_team.penalties} </br>
+            Vencedor: ${jogo.winner} </br>
             Localização: ${jogo.location} </br>
             Estádio: ${jogo.venue} </br>
-            </div>
+        </div>
+
     `
     }).join('');
 
-    document.getElementById('resultado').innerHTML = result;
+    console.log(result)
+
+    document.getElementById('resultado').innerHTML = result
+    
     })
 
 }
